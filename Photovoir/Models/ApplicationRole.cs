53cbf25199db61830b32fb257e474c5a5e8562d2
MICHAEL ALEXEY TUOHY <MICHAEL.TUOHY.2020@MUMAIL.IE>
2021-11-24 22:39:30 +0000

    Identity Finito

--- Photovoir/Models/ApplicationRole.cs
+++ Photovoir/Models/ApplicationRole.cs
@@ -1,13 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Photovoir.Models.DataModels
+namespace Photovoir.Models
 {
-    public class Role
+    public class ApplicationRole
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string NormalizedName { get; set; }
     }
 }
---
+++ Photovoir/Models/Manage/RemoveLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Photovoir.Models.Manage
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
